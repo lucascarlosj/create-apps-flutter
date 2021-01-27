2c50ab9badbed23bd4fe15b5927450a30e168e5d
Lucas Carlos <luc.cjov@gmail.com>
2021-01-27 16:52:56 -0300

    Share simples, app finalizado

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,7 +18,7 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if (_search == null)
+    if (_search == null || _search.isEmpty)
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=ENARn5cFYv78EqlS4vbKVBPro7UdveT1&limit=20&rating=g");
     else
@@ -50,7 +53,7 @@ class _HomePageState extends State<HomePage> {
             padding: EdgeInsets.all(10.0),
             child: TextField(
               decoration: InputDecoration(
-                  labelText: "Pesquise aqui!",
+                  labelText: "Pesquise aqui...",
                   prefixIcon: Icon(
                     Icons.search,
                   ),
@@ -111,14 +114,28 @@ class _HomePageState extends State<HomePage> {
             crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
         itemCount: _getCount(snapshot.data["data"]),
         itemBuilder: (context, index) {
-          if (_search == null || index < snapshot.data["data"].length)
+          if (_search == null || index < snapshot.data["data"].length) {
             return GestureDetector(
-                child: Image.network(
-              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-              height: 300.0,
-              fit: BoxFit.cover,
-            ));
-          else
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data["data"][index]["images"]["fixed_height"]
+                    ["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
+            );
+          } else
             return Container(
               child: GestureDetector(
                 child: Column(
